88df5bc20701b6e611f5af728e87aca0a6619350
DuckThuong <trinhthuong26022003@gmail.com>
2026-07-08 20:32:33 +0700

    update code

--- src/entities/sales/index.ts
+++
@@ -1,8 +0,0 @@
-export { TbPayment } from './payment.entity';
-export { TbRefund } from './refund.entity';
-export { TbBooking } from './booking.entity';
-export { TbCompanyStat } from './company-stat.entity';
-export { TbTripStat } from './trip-stat.entity';
-export { TbCommission } from './commission.entity';
-export { TbSettlement } from './settlement.entity';
-export { TbPromotionUsage } from './promotion-usage.entity';
